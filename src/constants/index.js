5b61b2d51f8700012237a613c35ac7767cdadd5d
Krish Kaustubh Deshmukh <deshmukh.102@buckeyemail.osu.edu>
2025-06-12 15:21:14 +0530

    final build

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,10 +11,10 @@ const navLinks = [
         name: "Skills",
         link: "#skills",
     },
-    {
-        name: "Testimonials",
-        link: "#testimonials",
-    },
+    // {
+    //     name: "Testimonials",
+    //     link: "#testimonials",
+    // },
 ];
 
 const words = [
@@ -30,8 +30,8 @@ const words = [
 
 const counterItems = [
     { value: 5, suffix: "+", label: "Years of Experience" },
-    { value: 200, suffix: "+", label: "Satisfied Clients" },
     { value: 10, suffix: "+", label: "Completed Projects" },
+    { value: 4, suffix: ".0", label: "GPA" },
     { value: 90, suffix: "%", label: "Client Retention Rate" },
 ];
 
@@ -202,64 +202,73 @@ const expLogos = [
 
 const testimonials = [
     {
-        name: "Esther Howard",
-        mentions: "@estherhoward",
+        name: "",
+        mentions: "",
         review:
-            "I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
+            "",
         imgPath: "/images/client1.png",
     },
     {
-        name: "Wade Warren",
-        mentions: "@wadewarren",
+        name: "",
+        mentions: "",
         review:
-            "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+            "",
         imgPath: "/images/client3.png",
     },
     {
-        name: "Guy Hawkins",
-        mentions: "@guyhawkins",
+        name: "",
+        mentions: "",
         review:
-            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+            "",
         imgPath: "/images/client2.png",
     },
     {
-        name: "Marvin McKinney",
-        mentions: "@marvinmckinney",
+        name: "",
+        mentions: "",
         review:
-            "Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
+            "",
         imgPath: "/images/client5.png",
     },
     {
-        name: "Floyd Miles",
-        mentions: "@floydmiles",
+        name: "",
+        mentions: "",
         review:
-            "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
+            "",
         imgPath: "/images/client4.png",
     },
     {
-        name: "Albert Flores",
-        mentions: "@albertflores",
+        name: "",
+        mentions: "",
         review:
-            "Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
+            "",
         imgPath: "/images/client6.png",
     },
 ];
 
 const socialImgs = [
+    // {
+    //     name: "insta",
+    //     url: "",
+    //     imgPath: "/images/insta.png",
+    // },
+    // {
+    //     name: "fb",
+    //     url: "",
+    //     imgPath: "/images/fb.png",
+    // },
+    // {
+    //     name: "x",
+    //     url: "",
+    //     imgPath: "/images/x.png",
+    // },
     {
-        name: "insta",
-        imgPath: "/images/insta.png",
-    },
-    {
-        name: "fb",
-        imgPath: "/images/fb.png",
-    },
-    {
-        name: "x",
-        imgPath: "/images/x.png",
+        name: "github",
+        url: "https://github.com/krishdeshmukhhh",
+        imgPath: "/images/github.png",
     },
     {
         name: "linkedin",
+        url: "https://www.linkedin.com/in/krish-deshmukh/",
         imgPath: "/images/linkedin.png",
     },
 ];
