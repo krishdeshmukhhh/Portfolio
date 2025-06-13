fbc79150046d3b00e57a8488056df86aa18cc8ad
Krish Kaustubh Deshmukh <deshmukh.102@buckeyemail.osu.edu>
2025-06-13 23:22:10 +0530

    add blog link

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer-container">
                 <div className="flex flex-col justify-center md:items-start items-center">
-                    <a href="/">Visit my blog</a>
+                    <a href="https://oia.osu.edu/news/2024/12/09/student-spotlight-krish-deshmukh">Visit my blog</a>
                 </div>
                 <div className="socials">
                     {socialImgs.map((img) => (
